3ef2a0ca85e3505e74beae7a0c0f4cf091494ef5
AshrafUzzaman04 <ashraf.uzzaman04082004@gmail.com>
2023-06-30 15:38:39 +0600

    read the total avg ratings with svg value

--- index.php
+++ index.php
@@ -51,12 +51,51 @@ include_once("./conn.php");
         $result = mysqli_query($conn, $avgQuery);
         $row = mysqli_fetch_object($result);
         $avgValue = $row->avg_value;
+        $starWidth = $avgValue * 20;
         if ($selctAll->num_rows > 0) {
         ?>
             <footer>
                 <div>
-                    <i class="fa-solid fa-star" style="color: #00596b;"></i>
+                    <div class="mx-auto jq-ry-container" style="width: max-content;">
+                        <div class="jq-ry-group-wrapper">
+                            <div class="jq-ry-normal-group jq-ry-group">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 12.705 512 486.59" x="0px" y="0px" xml:space="preserve" width="20px" height="32px" fill="gray">
+                                    <polygon points="256.814,12.705 317.205,198.566 512.631,198.566 354.529,313.435 414.918,499.295 256.814,384.427 98.713,499.295 159.102,313.435 1,198.566 196.426,198.566 "></polygon>
+                                </svg>
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 12.705 512 486.59" x="0px" y="0px" xml:space="preserve" width="20px" height="32px" fill="gray" style="margin-left: 1px;">
+                                    <polygon points="256.814,12.705 317.205,198.566 512.631,198.566 354.529,313.435 414.918,499.295 256.814,384.427 98.713,499.295 159.102,313.435 1,198.566 196.426,198.566 "></polygon>
+                                </svg>
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 12.705 512 486.59" x="0px" y="0px" xml:space="preserve" width="20px" height="32px" fill="gray" style="margin-left: 1px;">
+                                    <polygon points="256.814,12.705 317.205,198.566 512.631,198.566 354.529,313.435 414.918,499.295 256.814,384.427 98.713,499.295 159.102,313.435 1,198.566 196.426,198.566 "></polygon>
+                                </svg>
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 12.705 512 486.59" x="0px" y="0px" xml:space="preserve" width="20px" height="32px" fill="gray" style="margin-left: 1px;">
+                                    <polygon points="256.814,12.705 317.205,198.566 512.631,198.566 354.529,313.435 414.918,499.295 256.814,384.427 98.713,499.295 159.102,313.435 1,198.566 196.426,198.566 "></polygon>
+                                </svg>
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 12.705 512 486.59" x="0px" y="0px" xml:space="preserve" width="20px" height="32px" fill="gray" style="margin-left: 1px;">
+                                    <polygon points="256.814,12.705 317.205,198.566 512.631,198.566 354.529,313.435 414.918,499.295 256.814,384.427 98.713,499.295 159.102,313.435 1,198.566 196.426,198.566 "></polygon>
+                                </svg>
+                            </div>
+                            <div class="jq-ry-rated-group jq-ry-group" style="width: <?= $starWidth . "%" ?? null ?>;">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 12.705 512 486.59" x="0px" y="0px" xml:space="preserve" width="20px" height="32px" fill="#00596b">
+                                    <polygon points="256.814,12.705 317.205,198.566 512.631,198.566 354.529,313.435 414.918,499.295 256.814,384.427 98.713,499.295 159.102,313.435 1,198.566 196.426,198.566 "></polygon>
+                                </svg>
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 12.705 512 486.59" x="0px" y="0px" xml:space="preserve" width="20px" height="32px" fill="#00596b" style="margin-left: 1px;">
+                                    <polygon points="256.814,12.705 317.205,198.566 512.631,198.566 354.529,313.435 414.918,499.295 256.814,384.427 98.713,499.295 159.102,313.435 1,198.566 196.426,198.566 "></polygon>
+                                </svg>
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 12.705 512 486.59" x="0px" y="0px" xml:space="preserve" width="20px" height="32px" fill="#00596b" style="margin-left: 1px;">
+                                    <polygon points="256.814,12.705 317.205,198.566 512.631,198.566 354.529,313.435 414.918,499.295 256.814,384.427 98.713,499.295 159.102,313.435 1,198.566 196.426,198.566 "></polygon>
+                                </svg>
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 12.705 512 486.59" x="0px" y="0px" xml:space="preserve" width="20px" height="32px" fill="#00596b" style="margin-left: 1px;">
+                                    <polygon points="256.814,12.705 317.205,198.566 512.631,198.566 354.529,313.435 414.918,499.295 256.814,384.427 98.713,499.295 159.102,313.435 1,198.566 196.426,198.566 "></polygon>
+                                </svg>
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 12.705 512 486.59" x="0px" y="0px" xml:space="preserve" width="20px" height="32px" fill="#00596b" style="margin-left: 1px;">
+                                    <polygon points="256.814,12.705 317.205,198.566 512.631,198.566 354.529,313.435 414.918,499.295 256.814,384.427 98.713,499.295 159.102,313.435 1,198.566 196.426,198.566 "></polygon>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
                     <h4><?= round($avgValue, 1) ?? null ?></h4>
+
                     <span class="tooltiptext">Total Average Ratings!</span>
                 </div>
             </footer>
